ac12d94c967b15875c6d37e35cb879f375a68275
Philippe Muller <philippe.muller@gmail.com>
2018-03-04 15:27:08 +0800

    Add types

---
+++ types/prefixes.pp
@@ -0,0 +1,10 @@
+type AWS_Firewall::Prefixes = Hash[
+  AWS_Firewall::Region,
+  Hash[
+    AWS_Firewall::Service,
+    Struct[{
+      4 => Array[IP::Address::V4::CIDR],
+      6 => Array[IP::Address::V6::CIDR],
+    }],
+  ],
+]
---
+++ types/service.pp
@@ -0,0 +1,9 @@
+type AWS_Firewall::Service = Enum[
+  'AMAZON',
+  'CLOUDFRONT',
+  'CODEBUILD',
+  'EC2',
+  'ROUTE53',
+  'ROUTE53_HEALTHCHECKS',
+  'S3',
+]
