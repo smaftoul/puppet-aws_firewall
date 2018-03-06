52b7a67b294d097f5ab6d751ac9e22514d61976f
Philippe Muller <philippe.muller@gmail.com>
2018-03-06 10:40:04 +0800

    Add defined type aws_firewall::ipset

---
+++ manifests/ipset.pp
@@ -0,0 +1,22 @@
+# aws_firewall::ipset
+#
+# Create an IP set containing selected AWS IP networks.
+#
+# @example
+#   aws_firewall::ipset { 'ap-south-1-s3':
+#     regions  => ['ap-south-1'],
+#     services => ['S3'],
+#   }
+#
+define aws_firewall::ipset(
+  AWS_Firewall::Regions $regions,
+  AWS_Firewall::Services $services,
+  AWS_Firewall::IP_Version $ip_version,
+  Enum['present', 'absent'] $ensure = 'present',
+) {
+  ipset { $title:
+    ensure => $ensure,
+    set    => aws_firewall::service::prefixes($regions, $services, $ip_version),
+    type   => 'hash:net',
+  }
+}
---
+++ spec/defines/ipset_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe 'aws_firewall::ipset' do
+  context 'RedHat 7' do
+    let(:title) { 'ap-south-1-s3' }
+    let(:params) do
+      {
+        regions: ['ap-south-1'],
+        services: ['S3'],
+        ip_version: 4,
+      }
+    end
+    let :facts do
+      {
+        osfamily: 'RedHat',
+        operatingsystemmajrelease: '7',
+      }
+    end
+    it do
+      is_expected.to contain_ipset('ap-south-1-s3').only_with(
+        ensure: 'present',
+        set: %w[
+          52.92.248.0/22
+          52.219.62.0/23
+          52.219.64.0/22
+        ],
+        type: 'hash:net',
+        options: {},
+        ignore_contents: false,
+        keep_in_sync: true,
+      )
+    end
+  end
+end
